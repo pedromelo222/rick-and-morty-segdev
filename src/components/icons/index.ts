d303c84b62f28ba78e0dbc39cb16aefbb1edcd46
Pedro Melo <pedro.meloo@hotmail.com>
2022-07-10 15:43:34 -0300

    feat(card): character card component

---
+++ src/components/icons/index.ts
@@ -0,0 +1,15 @@
+import IconSkull from './IconSkull.vue'
+import IconHeart from './IconHeart.vue'
+import IconQuestion from './IconQuestion.vue'
+import IconGenderMale from './IconGenderMale.vue'
+import IconGenderFemale from './IconGenderFemale.vue'
+import IconGenderless from './IconGenderless.vue'
+
+export {
+  IconSkull,
+  IconHeart,
+  IconQuestion,
+  IconGenderMale,
+  IconGenderFemale,
+  IconGenderless,
+}
