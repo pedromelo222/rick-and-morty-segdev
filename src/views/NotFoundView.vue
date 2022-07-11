9daab5b80e39dda9316197d7926e1179dd7c8d79
Pedro Melo <pedro.meloo@hotmail.com>
2022-07-11 11:35:58 -0300

    refactor: changed not-found view  style

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -4,7 +4,7 @@ import { IconArrowLeft } from '@/components/icons'
 </script>
 
 <template>
-  <div class="h-full w-full text-9xl sm:text-[10em] xl:text-[20em] text-center flex items-center text-white justify-center min-h-[400px] md:min-h-[500px] relative">
+  <div class="items-shadow h-full w-full text-[10em] xl:text-[22em] text-center flex items-center text-white justify-center min-h-[400px] md:min-h-[500px] relative">
     4 <img src="@/assets/pagenotfound.png" class="w-[200px] md:w-[250px] lg:w-[300px] xl:w-[400px] -mx-8 ">4
     <div class="absolute top-0 left-0 ml-2 mt-8 md:ml-4 md:mt-8 text-black">
       <AppButton
@@ -18,3 +18,10 @@ import { IconArrowLeft } from '@/components/icons'
     </div>
   </div>
 </template>
+
+<style>
+.items-shadow {
+
+   filter: drop-shadow(8px 8px 0px black);
+}
+</style>
