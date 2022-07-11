799f9b8a64bceb5ce7ca8b11044cdbc8f8a8aa6b
Pedro Melo <pedro.meloo@hotmail.com>
2022-07-11 16:28:12 -0300

    chore: readme and comments

--- src/composables/useDefinedApiList.ts
+++ src/composables/useDefinedApiList.ts
@@ -1,12 +1,6 @@
 /**
  * Composable para retonar a lista de 'gender' e 'status'.
- * O ideal seria a API ter uma rota para retonar a lista dos tipos
- * 'gender' e 'status' para não ter a necessidade de fazer hardcode da lista
- *
- * Um possível problema que pode acontecer é a API adicionar um novo tipo de 'gender' ou 'status'
- * e ser necessario a manutenção dessa composable para surtir efeito
  */
-
 import type { Ref } from 'vue'
 import type { Gender, Status } from '@/types'
 import { useIconFromType } from '@/composables'
